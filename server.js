2117f2548bd13714d425362d0fcfac8c66ae8903
ricky2020 <metawork@hotmail.com>
2022-12-30 17:23:40 -0300

    texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hola Social Suecia'))
+app.get('/', (req,res) => res.send('<h1>Express con HTML!!</h1>'))
 
 app.listen(3000);
 console.log('Server on port 3000');
