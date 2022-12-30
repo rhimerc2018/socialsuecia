799563b605d0543602f37f70a81d9ac1db1cc5a3
ricky2020 <metawork@hotmail.com>
2022-12-30 17:29:44 -0300

    nueva subida

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (req,res) => res.send('<h1>Express con HTML!!</h1>'))
 
-app.listen(3000);
-console.log('Server on port 3000');
+app.listen(8080);
+console.log('Server on port 8080');
 
